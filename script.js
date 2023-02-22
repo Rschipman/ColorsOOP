128c28f061a137eb88f566320fe103abe7e27e71
RChipman <rschipman@gmail.com>
2023-02-22 16:41:49 -0300

    javascript class

--- script.js
+++ script.js
@@ -22,29 +22,53 @@
 // }
 // const firstColor = makeColor(35, 255, 150);
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+
+// color1 = new Color(40, 255, 65);
+// color2 = new Color(0, 0, 0);
+
+// document.body.style.backgroundColor = color1.rgb();
+// document.body.style.backgroundColor = color1.rgba(0.1);
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
 }
-
-Color.prototype.rgb = function () {
-  const { r, g, b } = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
-
-Color.prototype.hex = function () {
-  const { r, g, b } = this;
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-
-Color.prototype.rgba = function (a = 1.0) {
-  const { r, g, b } = this;
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
-
-color1 = new Color(40, 255, 65);
-color2 = new Color(0, 0, 0);
-
-document.body.style.backgroundColor = color1.rgb();
-document.body.style.backgroundColor = color1.rgba(0.1);
+const c1 = new Color(255, 67, 89, "peachyRed");
