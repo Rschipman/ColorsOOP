a9c631f1ccf0cdb250ea262b8bcec1d390b36acf
RChipman <rschipman@gmail.com>
2023-02-25 14:36:42 -0300

    small change

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-class Cat {
-    constructor(name)
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating`;
+  }
+}
+
+class Cat extends Pet {
+  meow() {
+    return "Meow!!";
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return "Woof!!";
+  }
 }
